de826bea0af964dbbc53f385446b7e1fafa27ec3
Daniil Lozhkin <delaynore@gmail.com>
2025-04-14 00:57:20 +0400

    fix: separate interfaces

---
+++ src/Postwit.Application/ITagRepository.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Postwit.Domain;
+
+namespace Postwit.Application;
+
+public interface ITagRepository
+{
+    DbSet<Tag> Tags { get; }
+}
--- src/Postwit.Application/IUnitOfWork.cs
+++ src/Postwit.Application/IUnitOfWork.cs
@@ -1,16 +1,6 @@
-﻿using Microsoft.EntityFrameworkCore;
-using Postwit.Domain;
-
-namespace Postwit.Application;
+﻿namespace Postwit.Application;
 
 public interface IUnitOfWork
 {
-    DbSet<Tag> Tags { get; }
-
     Task<int> SaveChangesAsync(CancellationToken cancellationToken);
-}
-
-public interface ITagRepository
-{
-    DbSet<Tag> Tags { get; }
 }
