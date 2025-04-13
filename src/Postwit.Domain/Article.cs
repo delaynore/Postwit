138820c108d4d77b607d1b8a93ac61db6c49ca98
Daniil Lozhkin <delaynore@gmail.com>
2025-04-13 22:49:54 +0400

    feat: add domain entities

---
+++ src/Postwit.Domain/Article.cs
@@ -0,0 +1,22 @@
+﻿namespace Postwit.Domain;
+
+public sealed class Article
+{
+    public Guid Id { get; set; } = Guid.CreateVersion7();
+
+    public string Title { get; set; }
+
+    public string Content { get; set; }
+
+    public Guid AuthorId { get; set; }
+
+    public TimeSpan ReadTime { get; set; }
+
+    public string ImageUrl { get; set; }
+
+    public string Slug { get; set; }
+
+    public DateTime CreatedAtUtc { get; set; }
+
+    public DateTime UpdatedAtUtc { get; set; }
+}
---
+++ src/Postwit.Domain/Tag.cs
@@ -0,0 +1,10 @@
+﻿namespace Postwit.Domain;
+
+public sealed class Tag
+{
+    public Guid Id { get; set; } = Guid.CreateVersion7();
+
+    public required string Name { get; set; }
+
+    public string? Description { get; set; }
+}
