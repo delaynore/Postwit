d2305ffec97a19117723d7ab1111cbdb91b77a5c
Daniil Lozhkin <delaynore@gmail.com>
2025-04-18 00:03:26 +0400

    feat: draft article service interface

---
+++ src/Postwit.Application/IArticleService.cs
@@ -0,0 +1,17 @@
+﻿using ErrorOr;
+using Postwit.Domain;
+
+namespace Postwit.Application;
+
+public interface IArticleService
+{
+    Task<ErrorOr<Article>> GetArticleById(Guid articleId, CancellationToken cancellationToken);
+
+    Task<ErrorOr<List<Article>>> GetArticles(CancellationToken cancellationToken);
+
+    Task<ErrorOr<Created>> CreateArticle(CancellationToken cancellationToken);
+
+    Task<ErrorOr<Updated>> UpdateArticle(CancellationToken cancellationToken);
+
+    Task<ErrorOr<Deleted>> DeleteArticle(CancellationToken cancellationToken);
+}
