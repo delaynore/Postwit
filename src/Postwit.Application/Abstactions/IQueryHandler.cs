2be757498e1af5c7f3d6907e6b7480e6afbc8dd6
Daniil Lozhkin <delaynore@gmail.com>
2025-07-10 15:44:14 +0400

    feat: cqs abstractions

---
+++ src/Postwit.Application/Abstactions/IQueryHandler.cs
@@ -0,0 +1,8 @@
+﻿namespace Postwit.Application.Abstactions;
+
+public interface IQuery;
+
+public interface IQueryHandler<in TQuery, TResponse> where TQuery : IQuery
+{
+    Task<TResponse> Handle(TQuery query, CancellationToken cancellationToken);
+}
