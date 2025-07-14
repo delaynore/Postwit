8509617594336bcbcf2de392232014a5e45290fd
Daniil Lozhkin <delaynore@gmail.com>
2025-07-14 23:15:21 +0400

    feat: publish drafted article

---
+++ src/Postwit.Application/Articles/Commands/PublishDraftedArticle/PublishDraftedArticleCommand.cs
@@ -0,0 +1,4 @@
+﻿using Postwit.Application.Abstactions;
+
+namespace Postwit.Application.Articles.Commands.PublishDraftedArticle;
+public sealed record PublishDraftedArticleCommand(Guid ArticleId) : ICommand;
