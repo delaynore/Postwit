0309c3c6008fd5be07349d105081ff1d1042e731
Daniil Lozhkin <delaynore@gmail.com>
2025-07-16 23:40:09 +0400

    feat: add use cases in controller

--- src/Postwit.Api/Controllers/ArticlesController.cs
+++ src/Postwit.Api/Controllers/ArticlesController.cs
@@ -1,7 +1,8 @@
-﻿using ErrorOr;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Postwit.Application.Abstactions;
 using Postwit.Application.Articles.Commands.PublishArticle;
+using Postwit.Application.Articles.Commands.PublishDraftedArticle;
+using Postwit.Application.Articles.Commands.UpdateArticle;
 using Postwit.Application.Contracts.Articles;
 
 namespace Postwit.Api.Controllers;
@@ -20,6 +21,37 @@ public sealed class ArticlesController : ControllerBase
 
         return result.Match(
              s => Results.Ok(s),
-             e => Results.NotFound(e));
+             e => Results.BadRequest(e));
+    }
+
+    [HttpPut("{articleId}/publish")]
+    public async Task<IResult> PublishDraftedArticle(
+        [FromRoute] Guid articleId,
+        [FromServices] ICommandHandler<PublishDraftedArticleCommand> handler,
+        CancellationToken cancellationToken)
+    {
+        var command = new PublishDraftedArticleCommand(articleId);
+
+        var result = await handler.Handle(command, cancellationToken);
+
+        return result.Match(
+            s => Results.Ok(),
+            e => Results.BadRequest(e));
+    }
+
+    [HttpPut("{articleId}")]
+    public async Task<IResult> UpdateArticle(
+        [FromRoute] Guid articleId,
+        [FromBody] UpdateArticleDto dto,
+        [FromServices] ICommandHandler<UpdateArticleCommand> handler,
+        CancellationToken cancellationToken)
+    {
+        var command = new UpdateArticleCommand(articleId, dto);
+
+        var result = await handler.Handle(command, cancellationToken);
+
+        return result.Match(
+            s => Results.Ok(),
+            e => Results.BadRequest(e));
     }
 }
