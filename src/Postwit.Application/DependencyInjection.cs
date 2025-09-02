e378edab2a95982c38a544c61681386f8b992582
Daniil Lozhkin <delaynore@gmail.com>
2025-09-02 18:08:50 +0400

    feat: register article handlers

--- src/Postwit.Application/DependencyInjection.cs
+++ src/Postwit.Application/DependencyInjection.cs
@@ -1,6 +1,12 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using Postwit.Application.Abstactions;
+using Postwit.Application.Articles.Commands.DeleteArticle;
 using Postwit.Application.Articles.Commands.PublishArticle;
+using Postwit.Application.Articles.Commands.PublishDraftedArticle;
+using Postwit.Application.Articles.Commands.UpdateArticle;
+using Postwit.Application.Articles.Queries.GetArticle;
+using Postwit.Application.Articles.Queries.GetArticles;
+using Postwit.Application.Contracts.Articles;
 
 namespace Postwit.Application;
 
@@ -11,7 +17,14 @@ public static class DependencyInjection
         services.AddScoped<ITagService, TagService>();
 
         services.AddScoped<ICommandHandler<PublishArticleCommand, Guid>, PublishArticleHandler>();
-
+        services.AddScoped<ICommandHandler<PublishDraftedArticleCommand>, PublishDraftedArticleCommandHandler>();
+        
+        services.AddScoped<ICommandHandler<DeleteArticleCommand>, DeleteArticleHandler>();
+        services.AddScoped<ICommandHandler<UpdateArticleCommand>, UpdateArticleCommandHandler>();
+        
+        services.AddScoped<IQueryHandler<GetArticlesQuery, ArticleListResponse>, GetArticlesQueryHandler>();
+        services.AddScoped<IQueryHandler<GetArticleQuery, ErrorOr.ErrorOr<ArticleDto>>, GetArticleQueryHandler>();
+        
         return services;
     }
 }
